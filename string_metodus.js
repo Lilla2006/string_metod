c5684bf488fa9e97ebff5777d31916bf6eda8781
Lilla <98951774+Lilla2006@users.noreply.github.com>
2024-10-04 11:34:35 +0200

    Add files via upload

---
+++ string_metodus.js
@@ -0,0 +1,18 @@
+let txt = "oigjrgejhsdifvuhujefvuhnhd";
+let hossz = txt.length;
+console.log(hossz);
+
+console.log(txt.slice(0, 5));
+
+console.log(txt.slice(2, 8));
+
+console.log(txt.slice(4));
+
+console.log(txt.slice(4, 10));
+
+console.log(txt.toUpperCase());
+
+let e1 = txt.slice(6, 7);
+let e2 = txt.slice(18, 19);
+let nagye1 = e1.toUpperCase;
+let nagye2 = e2.toUpperCase;
